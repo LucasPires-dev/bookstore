3c36ec2981f52e8bdfb94a122e7dc69998734d80
lcpires7 <lcpires7.dev@gmail.com>
2025-06-26 11:21:59 -0300

    feat: configure pagination

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'order',
-    'product'
+    'product',
+    "django_extensions"
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}
